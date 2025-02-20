5ec3bcfb367427c37256f81c135ff7fd2d56e601
Alexandr_V <vasilyevsasha0195@gmail.com>
2025-02-21 02:40:58 +0600

    some work correctly

--- src/Main.java
+++ src/Main.java
@@ -45,10 +45,58 @@ public class Main {
     }
 
     private static void initRoutes(HttpServer server) {
-        server.createContext("/", exchange -> handleRequest(exchange));
+        server.createContext("/", exchange -> handleRequestHtml(exchange));
         server.createContext("/apps/", exchange -> handleRequest(exchange));
         server.createContext("/apps/profile", exchange -> handleRequest(exchange));
     }
+
+    private static void handleRequestHtml(HttpExchange exchange) {
+        try {
+            String method = exchange.getRequestMethod();
+            URI uri = exchange.getRequestURI();
+            String filePath = "src" + uri.getPath();
+            Path path = Paths.get(filePath);
+            String mimeType = getMimeType(path);
+
+            if (uri.getPath().contains(".html")) {
+                // Указываем относительный путь к файлу
+
+                if (Files.exists(path)) {
+                    String content = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+                    exchange.getResponseHeaders().add("Content-Type", mimeType + "; charset=utf-8");
+                    exchange.sendResponseHeaders(200, content.getBytes().length);
+                    try (OutputStream os = exchange.getResponseBody()) {
+                        os.write(content.getBytes(StandardCharsets.UTF_8));
+                    }
+                } else {
+                    String response = "File not found";
+                    exchange.sendResponseHeaders(404, response.getBytes().length);
+                    try (OutputStream os = exchange.getResponseBody()) {
+                        os.write(response.getBytes(StandardCharsets.UTF_8));
+                    }
+                }
+            } else {
+                exchange.getResponseHeaders().add("Content-Type", "text/plain; charset=utf-8");
+                int responseCode = 200;
+                int length = 0;
+                exchange.sendResponseHeaders(responseCode, length);
+
+                try (PrintWriter writer = getWriterFrom(exchange)) {
+                    String pathString = exchange.getHttpContext().getPath();
+                    write(writer, "HTTP method", method);
+                    write(writer, "Request", uri.toString());
+                    write(writer, "Handler", pathString);
+                    writeHeaders(writer, "Request headers", exchange.getRequestHeaders());
+                    writeData(writer, exchange);
+                    writer.flush();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
     private static void handleRequest(HttpExchange exchange) {
         try {
@@ -73,6 +121,24 @@ public class Main {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void sendHtmlFile(HttpExchange exchange, String filePath) throws IOException {
+        Path path = Paths.get(filePath);
+        if (Files.exists(path)) {
+            String content = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+            exchange.getResponseHeaders().add("Content-Type", "text/html; charset=utf-8");
+            exchange.sendResponseHeaders(200, content.getBytes().length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(content.getBytes(StandardCharsets.UTF_8));
+            }
+        } else {
+            String response = "File not found";
+            exchange.sendResponseHeaders(404, response.getBytes().length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes(StandardCharsets.UTF_8));
+            }
         }
     }
 
@@ -111,6 +177,17 @@ public class Main {
             reader.lines().forEach(line -> write(writer, "\t", line));
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static String getMimeType(Path filePath) throws IOException {
+        String fileName = filePath.getFileName().toString();
+        if (fileName.endsWith(".css")) {
+            return "text/css";
+        } else if (fileName.endsWith(".html")) {
+            return "text/html";
+        } else {
+            return "text/plain";
         }
     }
 
